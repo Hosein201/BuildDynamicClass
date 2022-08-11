6ce72a647c2d2ba33adb414c5fba59a8315fa4ea
Hossein Golmohammadi <42300369+Hosein201@users.noreply.github.com>
2022-08-11 15:19:34 +0430

    Add project files.

---
+++ CreateClassWithReflection/MyClassBuilder.cs
@@ -0,0 +1,107 @@
+﻿using System.Reflection;
+using System.Reflection.Emit;
+
+namespace CreateClassWithReflection
+{
+    public class MyClassBuilder
+    {
+        AssemblyName asemblyName;
+        public MyClassBuilder(string ClassName)
+        {
+            this.asemblyName = new AssemblyName(ClassName);
+        }
+
+        /// <summary>
+        /// build dynamic class in run time
+        /// </summary>
+        /// <param name="propertyNames"> names of props </param>
+        /// <param name="types"> types of props </param>
+        /// <param name="values">values of props</param>
+        /// <returns></returns>
+        public object BuildDynamicClass(string[] propertyNames, Type[] types, object[] values)
+        {
+            if (propertyNames.Length != types.Length || propertyNames.Length != values.Length || types.Length != values.Length)
+                throw new ArgumentOutOfRangeException("The number of property names should match their corresopnding types number and values number ");
+
+            var dynamicClass = CreateObject(propertyNames, types);
+
+            for (int i = 0; i < propertyNames.Length; i++)
+            {
+                PropertyInfo propertyInfo = dynamicClass.GetType().GetProperty(propertyNames[i]);
+                propertyInfo.SetValue(dynamicClass, values[i], null);
+            }
+
+            return dynamicClass;
+        }
+
+        private object CreateObject(string[] propertyNames, Type[] types)
+        {
+            TypeBuilder DynamicClass = CreateClass();
+            CreateConstructor(DynamicClass);
+
+            for (int ind = 0; ind < propertyNames.Count(); ind++)
+                CreateProperty(DynamicClass, propertyNames[ind], types[ind]);
+
+            Type type = DynamicClass.CreateType();
+
+            return Activator.CreateInstance(type);
+        }
+
+        private TypeBuilder CreateClass()
+        {
+            AssemblyBuilder assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(this.asemblyName, AssemblyBuilderAccess.RunAndCollect);
+
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("DynamicClass");
+
+            TypeBuilder typeBuilder = moduleBuilder.DefineType(this.asemblyName.FullName
+                                , TypeAttributes.Public |
+                                TypeAttributes.Class |
+                                TypeAttributes.AutoClass |
+                                TypeAttributes.AnsiClass |
+                                TypeAttributes.BeforeFieldInit |
+                                TypeAttributes.AutoLayout
+                                , null);
+            return typeBuilder;
+        }
+
+        private void CreateConstructor(TypeBuilder typeBuilder)
+        {
+            typeBuilder.DefineDefaultConstructor(MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.RTSpecialName);
+        }
+
+        private void CreateProperty(TypeBuilder typeBuilder, string propertyName, Type propertyType)
+        {
+            FieldBuilder fieldBuilder = typeBuilder.DefineField("_" + propertyName, propertyType, FieldAttributes.Private);
+
+            PropertyBuilder propertyBuilder = typeBuilder.DefineProperty(propertyName, PropertyAttributes.HasDefault, propertyType, null);
+            MethodBuilder getPropMthdBldr = typeBuilder.DefineMethod("get_" + propertyName, MethodAttributes.Public, propertyType, Type.EmptyTypes);
+
+            ILGenerator getIl = getPropMthdBldr.GetILGenerator();
+            getIl.Emit(OpCodes.Ldarg_0);
+            getIl.Emit(OpCodes.Ldfld, fieldBuilder);
+            getIl.Emit(OpCodes.Ret);
+
+            MethodBuilder setPropMthdBldr = typeBuilder.DefineMethod("set_" + propertyName,
+                  MethodAttributes.Public |
+                  MethodAttributes.SpecialName |
+                  MethodAttributes.HideBySig,
+                  null, new[] { propertyType });
+
+            ILGenerator setIl = setPropMthdBldr.GetILGenerator();
+            Label modifyProperty = setIl.DefineLabel();
+            Label exitSet = setIl.DefineLabel();
+
+            setIl.MarkLabel(modifyProperty);
+            setIl.Emit(OpCodes.Ldarg_0);
+            setIl.Emit(OpCodes.Ldarg_1);
+            setIl.Emit(OpCodes.Stfld, fieldBuilder);
+
+            setIl.Emit(OpCodes.Nop);
+            setIl.MarkLabel(exitSet);
+            setIl.Emit(OpCodes.Ret);
+
+            propertyBuilder.SetGetMethod(getPropMthdBldr);
+            propertyBuilder.SetSetMethod(setPropMthdBldr);
+        }
+    }
+}
---
+++ CreateClassWithReflection/Program.cs
@@ -0,0 +1,23 @@
+﻿using CreateClassWithReflection;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            MyClassBuilder myClassBuilder = new MyClassBuilder("Person");
+
+            var dynamicClass = myClassBuilder.BuildDynamicClass(
+                new string[] { "id", "name" },
+                new Type[] { typeof(int) },
+                new object[] { 1, "Hossein" });
+
+            Console.ReadKey();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
